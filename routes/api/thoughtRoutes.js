713388aad0d256068af9cac5bb30214a16674eb1
rtrevill <139879271+rtrevill@users.noreply.github.com>
2024-01-04 23:42:07 +1100

    completed routes for reactions

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -8,9 +8,16 @@ const {
     modifyThought,
 } = require('../../controllers/thoughtController.js');
 
+const {
+    newReaction,
+    delReaction,
+} = require('../../controllers/reactionController.js');
+
 router.route('/').get(getThoughts).post(newThought);
 
 router.route('/:thoughtId').get(singleThought).delete(deleteThought).put(modifyThought);
+
+router.route('/:thoughtId/reactions').post(newReaction).delete(delReaction);
 
 
 router.use('/', (req,res) => {
