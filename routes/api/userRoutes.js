feda4a944504cc4b7c491eed630a77396571056f
rtrevill <139879271+rtrevill@users.noreply.github.com>
2024-01-04 14:38:02 +1100

    added further user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,10 +2,13 @@ const router = require('express').Router();
 
 const {
     getUsers,
+    getSingleUser,
+    createUser,
 } = require('../../controllers/userController.js');
 
-router.route('/').get(getUsers);
+router.route('/').get(getUsers).post(createUser);
 
+router.route('/:userId').get(getSingleUser);
 // router.route('/', (req,res) => {
 //     res.send("What's Wrong?");
 // })
